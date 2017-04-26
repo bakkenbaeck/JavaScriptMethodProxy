75cf4dbc86f038c16b866a925736da2783c52f74
Elvis Nuñez <elvisnunez@me.com>
2017-04-26 13:09:27 +0200

    Remove unused WebMover

--- ObjectivelyHybridiOSExample/ViewController.m
+++ ObjectivelyHybridiOSExample/ViewController.m
@@ -22,7 +22,6 @@
  */
 
 #import "ViewController.h"
-#import "WebMover.h"
 
 @interface ViewController ()
 @property (strong, nonatomic)WKWebView *appWebView;
@@ -32,29 +31,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    /*
-     * This line of code doesn't work in an Objective-C project. This appears to be a defect. The web files need to be
-     * moved into the application's temp directory.
-     
-     NSString *indexHTMLPath = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
-     
-     */
-    
-    NSError *moveError = nil;
-    //modify the array of file types to fit the web file types your app uses.
-    NSString *indexHTMLPath = [WebMover moveDirectoriesAndWebFilesOfType:@[@"js",@"css",@"html",@"png",@"jpg",@"gif"] error:&moveError];
-    if (moveError != nil) {
-        NSLog(@"%@",moveError.description);
-    }
+
     WKWebViewConfiguration *theConfiguration = [[WKWebViewConfiguration alloc] init];
     [theConfiguration.userContentController addScriptMessageHandler:self name:@"interOp"];
     _appWebView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:theConfiguration];
-    
-    //The Objective-C URLWithString creates a URL that fails to load
-    //when used in a NSURLRequest that is later consumed by
-    //NSURL *url = [NSURL URLWithString:indexHTMLPath];
 
-    NSURL *url = [NSURL fileURLWithPath:indexHTMLPath];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
+    NSURL *url = [NSURL fileURLWithPath:path];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [_appWebView loadRequest:request];
     [self.view addSubview:_appWebView];
