a7ecffbb503a964f29cce693b4e9536b9c434587
Elvis Nuñez <elvisnunez@me.com>
2017-04-26 13:36:30 +0200

    Implement working method

---
+++ ObjectivelyHybridiOSExample/AppDelegate.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder {
+    var window: UIWindow?
+}
+
+extension AppDelegate: UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        guard let window = self.window else { fatalError("Window not found") }
+
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+
+        return true
+    }
+}
---
+++ ObjectivelyHybridiOSExample/ViewController.swift
@@ -0,0 +1,44 @@
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    lazy var webView: WKWebView = {
+        let configuration = WKWebViewConfiguration()
+        configuration.userContentController.add(self, name: "interOp")
+
+        let view = WKWebView(frame: self.view.frame, configuration: configuration)
+        view.scrollView.isScrollEnabled = true
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.addSubview(self.webView)
+
+        let path = Bundle.main.path(forResource: "index", ofType: "html")!
+        let url = URL(fileURLWithPath: path)
+        let request = URLRequest(url: url)
+        self.webView.load(request)
+    }
+}
+
+extension ViewController: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if let sentData = message.body as? [String: Any] {
+            if let count = sentData["count"] as? Int {
+                self.webView.evaluateJavaScript("storeAndShow(\(count + 1))") { jsReturnValue, error in
+                    if let error = error {
+                        print("Error: \(error)")
+                    } else if let newCount = jsReturnValue as? Int {
+                        print("Returned value: \(newCount)")
+                    } else {
+                        print("No return from JS")
+                    }
+                }
+            }
+        }
+    }
+}
